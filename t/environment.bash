dc262fb73a5c32e7909ae74d56aa4e7828ac2e04
Devin J. Pohly <djpohly@gmail.com>
2017-05-25 01:42:44 -0500

    remove some vestigial helper functions

--- t/environment.bash
+++ t/environment.bash
@@ -1,13 +1,11 @@
 # Command to run for vcsh (test-lib will put build dir at the head of PATH)
-# To test other shells: "dash vcsh", "zsh vcsh", etc.
-export VCSH="vcsh"
+# To test other shells, run with VCSH="dash vcsh", VCSH="zsh vcsh", etc.
+: "${VCSH:=vcsh}"
+export VCSH
 
 # XXX Currently the /etc/vcsh/config file can affect testcases.
 # Perhaps it should be ignored if one exists in $XDG_CONFIG_HOME or was
 # specified with -c?
-
-# Other things used in tests
-export GITVERSION=$(git version)
 
 # Clear out environment variables that affect VCSH behavior
 unset VCSH_OPTION_CONFIG VCSH_REPO_D VCSH_HOOK_D VCSH_OVERLAY_D
@@ -20,41 +18,6 @@ find_gitrepos() {
 
 num_gitrepos() {
 	find_gitrepos "$@" | wc -l
-}
-
-assert() {
-	if [ $# -ne 3 ]; then
-		echo 'assert: requires three arguments (forgot to quote?)' >&2
-		return 2
-	fi
-
-	if ! test "$@"; then
-		echo "assertion \"$2\" failed" >&2
-		echo "actual   : \"$1\"" >&2
-		echo "reference: \"$3\"" >&2
-		return 1
-	fi
-}
-
-assert_file() {
-	negate=
-	if [ "$1" = "!" ]; then
-		if [ $# -ne 3 ]; then
-			echo 'assert_file: requires two arguments after ! (forgot to quote?)' >&2
-			return 2
-		fi
-		negate='! '
-		shift
-	elif [ $# -ne 2 ]; then
-		echo 'assert_file: requires two arguments (forgot to quote?)' >&2
-		return 2
-	fi
-
-	if ! test $negate "$@"; then
-		echo "assertion \"$negate$1\" failed" >&2
-		echo "file: \"$2\"" >&2
-		return 1
-	fi
 }
 
 # Similar to grep -q, but prints the entire input to stderr for debugging
